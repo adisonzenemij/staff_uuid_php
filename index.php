35fec81f225b78596629cfd2ce62341edbf31314
adisonzenemij <adisonzenemij@gmail.com>
2024-10-18 18:06:03 -0500

    feat: proyecto desarrollado

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+    # Directorio Proyecto
+    define('DIRSTORAGE', __DIR__);
+    echo '<span style="color: blue;">Directorio:</span>' . DIRSTORAGE . '<br/>';
+    require DIRSTORAGE . '/vendor/autoload.php';
+    
